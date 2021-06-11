66a5bd2078aaacb1eee259ad506c94d9a729e647
Gerardo Roza <rozagerardo@gmail.com>
2021-06-11 16:54:25 -0300

    Added using-query and named-queries lesson modules

---
+++ using-query-end/src/main/java/com/baeldung/lsd/UsingQueryApp.java
@@ -0,0 +1,30 @@
+package com.baeldung.lsd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.baeldung.lsd.persistence.repository.IProjectRepository;
+
+@SpringBootApplication
+public class UsingQueryApp implements ApplicationRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsingQueryApp.class);
+
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    public static void main(final String... args) {
+        SpringApplication.run(UsingQueryApp.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+}
