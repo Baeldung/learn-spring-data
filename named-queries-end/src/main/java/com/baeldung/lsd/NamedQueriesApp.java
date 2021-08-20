36628a5011d3fb966ee3ff07d8aee925aaccafc3
Gerardo Roza <rozagerardo@gmail.com>
2021-08-20 19:01:53 -0300

    Improved variable names for Named Queries lesson

--- named-queries-end/src/main/java/com/baeldung/lsd/NamedQueriesApp.java
+++ named-queries-end/src/main/java/com/baeldung/lsd/NamedQueriesApp.java
@@ -33,24 +33,24 @@ public class NamedQueriesApp implements ApplicationRunner {
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
 
-        List<Project> result = entityManager.createNamedQuery("namedQueryProjectsWithIdGreaterThan", Project.class)
+        List<Project> projectsWithIdGt1 = entityManager.createNamedQuery("namedQueryProjectsWithIdGreaterThan", Project.class)
             .setParameter("id", 1L)
             .getResultList();
-        LOG.info("Find Projects with Id greater than 1 using EntityManager:\n{}", result);
+        LOG.info("Find Projects with Id greater than 1 using EntityManager:\n{}", projectsWithIdGt1);
 
-        List<Project> result1 = projectRepository.findProjectsWithIdLessThan(3L);
-        LOG.info("Find Projects with Id less than 3:\n{}", result1);
+        List<Project> projectsWithIdLt3 = projectRepository.findProjectsWithIdLessThan(3L);
+        LOG.info("Find Projects with Id less than 3:\n{}", projectsWithIdLt3);
 
         projectRepository.updateProjectDescriptionById(1L, "New description updated by named query");
         Project project1 = projectRepository.findById(1L)
             .get();
         LOG.info("After updating the description of the Project(id=1):\n{}", project1);
 
-        List<Project> result2 = projectRepository.findProjectsWithDescriptionShorterThan(16);
-        LOG.info("Find Projects with description shorter than 16:\n{}", result2);
+        List<Project> projectsWithShortDescription = projectRepository.findProjectsWithDescriptionShorterThan(16);
+        LOG.info("Find Projects with description shorter than 16:\n{}", projectsWithShortDescription);
 
-        List<Project> result3 = projectRepository.findProjectsWithDescriptionPrefix("About");
-        LOG.info("Find Projects with Description Prefix (NamedQuery from properties file):\n{}", result3);
+        List<Project> projectsWithDescriptionPrefix = projectRepository.findProjectsWithDescriptionPrefix("About");
+        LOG.info("Find Projects with Description Prefix (NamedQuery from properties file):\n{}", projectsWithDescriptionPrefix);
 
     }
 
