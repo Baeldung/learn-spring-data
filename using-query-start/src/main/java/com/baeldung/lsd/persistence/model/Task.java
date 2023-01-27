5eef2423a26cd38db7c3b231b16bf3091ce1846e
Ger Roza <rozagerardo@gmail.com>
2023-01-27 11:49:44 -0300

    Upgraded to Boot 3

--- using-query-start/src/main/java/com/baeldung/lsd/persistence/model/Task.java
+++ using-query-start/src/main/java/com/baeldung/lsd/persistence/model/Task.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 import java.util.Objects;
 import java.util.UUID;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Task {
