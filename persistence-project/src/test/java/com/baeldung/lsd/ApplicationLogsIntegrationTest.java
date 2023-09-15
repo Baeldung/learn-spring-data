7a45dfde05b8f6b96e9d98e0e32221c16726c52f
Ger Roza <rozagerardo@gmail.com>
2023-09-15 12:03:21 -0300

    Upgraded to Boot 3.1 and added log tests to check lesson functionality

---
+++ persistence-project/src/test/java/com/baeldung/lsd/ApplicationLogsIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.baeldung.lsd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.baeldung.lsd.utils.LoggerListAppender;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+@SpringBootTest
+class ApplicationLogsIntegrationTest {
+
+    @BeforeAll
+    public static void clearLogList() {
+        LoggerListAppender.clearEventList();
+    }
+
+    @Test
+    void whenApplicationIsStarted_thenLogsContainExpectedText() throws InterruptedException {
+        Thread.sleep(500);
+        assertThat(LoggerListAppender.getEvents()).haveAtLeastOne(eventContains("Starting Spring Boot application..."));
+    }
+
+    private Condition<ILoggingEvent> eventContains(String substring) {
+        return new Condition<ILoggingEvent>(entry -> (substring == null || (entry.getFormattedMessage() != null && entry.getFormattedMessage()
+            .contains(substring))), String.format("entry with message '%s'", substring));
+    }
+}
--- persistence-project/src/test/java/com/baeldung/lsd/SpringContextIntegrationTest.java
+++ persistence-project/src/test/java/com/baeldung/lsd/SpringContextIntegrationTest.java
@@ -2,11 +2,14 @@ package com.baeldung.lsd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext
 class SpringContextIntegrationTest {
 
     @Test
     void whenContextIsLoaded_thenNoExceptions() {
     }
+
 }
