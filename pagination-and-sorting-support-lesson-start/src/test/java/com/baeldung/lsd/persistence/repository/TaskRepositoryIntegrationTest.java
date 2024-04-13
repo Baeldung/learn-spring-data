24c3d989cdd19a71fd50112ad3c4edcb49bfed65
Ger Roza <rozagerardo@gmail.com>
2024-04-12 21:23:13 -0300

    Upgraded to Boot 3.2

--- pagination-and-sorting-support-lesson-start/src/test/java/com/baeldung/lsd/persistence/repository/TaskRepositoryIntegrationTest.java
+++ pagination-and-sorting-support-lesson-start/src/test/java/com/baeldung/lsd/persistence/repository/TaskRepositoryIntegrationTest.java
@@ -9,9 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import com.baeldung.lsd.persistence.model.Project;
 import com.baeldung.lsd.persistence.model.Task;
